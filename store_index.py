2d861c603d436d373091bc7a8306a7df6189660a
Sailendri <51899206+Sailendri@users.noreply.github.com>
2024-11-17 09:32:22 -0800

    Update store_index.py

--- store_index.py
+++ store_index.py
@@ -18,11 +18,18 @@ embeddings = download_hugging_face_embeddings()
 
 
 #Initializing the Pinecone
-pinecone.init(api_key=PINECONE_API_KEY,
-              environment=PINECONE_API_ENV)
+pinecone.init(api_key=PINECONE_API_KEY)
+              #environment=PINECONE_API_ENV)
 
 
 index_name="medical-bot"
+from langchain_pinecone import PineconeVectorStore
+vector_store = PineconeVectorStore(index=index, embedding=embeddings)
+from uuid import uuid4
+from langchain_core.documents import Document
+uuids = [str(uuid4()) for _ in range(len(text_chunks))]
+vector_store.add_documents(documents=text_chunks, ids=uuids)
+
 
 #Creating Embeddings for Each of The Text Chunks & storing
 #docsearch=Pinecone.from_texts([t.page_content for t in text_chunks], embeddings, index_name=index_name)
